63145e7946b80f4d8794b0c509cec1153f655bff
Peter Whittle <peter.whittle@city-holdings.co.uk>
2019-05-13 13:57:33 +0100

    Created coordinator protocols.

---
+++ L5RBuilder/Protocols/Coordinator.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator.swift
+//  L5RBuilder
+//
+//  Created by Peter Whittle on 13/05/2019.
+//  Copyright © 2019 Peter Whittle. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+}
---
+++ L5RBuilder/Protocols/Storyboarded.swift
@@ -0,0 +1,24 @@
+//
+//  Storyboarded.swift
+//  L5RBuilder
+//
+//  Created by Peter Whittle on 13/05/2019.
+//  Copyright © 2019 Peter Whittle. All rights reserved.
+//
+
+import UIKit
+
+protocol Storyboarded {
+    static func instantiate() -> Self
+}
+
+extension Storyboarded where Self: UIViewController {
+    
+    static func instantiate() -> Self {
+        let fullName = NSStringFromClass(self)
+        let className = fullName.components(separatedBy: ".")[1]
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        return storyboard.instantiateViewController(withIdentifier: className) as! Self
+    }
+}
