9b0113b37be614b78f64e240ec295da338a4c335
Peter Whittle <peter.whittle@city-holdings.co.uk>
2019-08-21 11:51:47 +0100

    Added code so that total number of cards in deck is shown in the tab view

--- L5RBuilder/VCs/DeckbuilderVCs/Dynasty/DynastyDeckBuilderVC.swift
+++ L5RBuilder/VCs/DeckbuilderVCs/Dynasty/DynastyDeckBuilderVC.swift
@@ -15,10 +15,12 @@ class DynastyDeckBuilderVC: UITableViewController, CardViewDelegate, CardCellDel
     let db = DBHelper.sharedInstance
     var deck: Deck
     var availableCards: Results<Card>
+    var dynastyDeckCardCount: Int
     
     init(deck: Deck) {
         self.deck = deck
         availableCards = db.getClanDynastyCards(clan: deck.clan!)
+        self.dynastyDeckCardCount = 0
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -29,7 +31,7 @@ class DynastyDeckBuilderVC: UITableViewController, CardViewDelegate, CardCellDel
     override func viewDidLoad() {
         self.tableView.reloadData()
         super.viewDidLoad()
-        self.tabBarItem = UITabBarItem(title: "Dynasty", image: UIImage(named: "dynastyDeckIcon"), tag: 0)
+        self.tabBarItem = UITabBarItem(title: "Dynasty(\(dynastyDeckCardCount))", image: UIImage(named: "dynastyDeckIcon"), tag: 0)
         
         let deckbuilderNib = UINib(nibName: "DeckBuilderCardTableViewCell", bundle: nil)
         self.tableView.register(deckbuilderNib, forCellReuseIdentifier: "DeckBuilderCardTableViewCell")
@@ -52,8 +54,8 @@ extension DynastyDeckBuilderVC {
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: "DeckBuilderCardTableViewCell", for: indexPath) as? DeckBuilderCardTableViewCell {
             let card = availableCards[indexPath.row]
-            let cardCount = self.deck.dynastyDeck.filter({$0.id == card.id}).count
-            cell.setUpCell(indexPath: indexPath, availableCards: self.availableCards, cardCount: cardCount)
+            let cardCount = deck.dynastyDeck.filter({$0.id == card.id}).count
+            cell.setUpCell(indexPath: indexPath, availableCards: availableCards, cardCount: cardCount)
             cell.delegate = self
             return cell
         }
@@ -65,50 +67,57 @@ extension DynastyDeckBuilderVC {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         let selectedCard = availableCards[indexPath.row]
-        let selectedCardsInDeckCount = self.deck.dynastyDeck.filter({$0.id == selectedCard.id}).count
+        let selectedCardsInDeckCount = deck.dynastyDeck.filter({$0.id == selectedCard.id}).count
         print("I clicked a card to edit. There are currently \(selectedCardsInDeckCount) copies of \(selectedCard.id) in the dynasty deck.")
         coordinator?.showDynastyCard(selectedCard: selectedCard, delegate: self, cardsInDeckCount: selectedCardsInDeckCount)
     }
     
     func addCards(card: Card, numberOfCards: Int) {
         
-        let selectedCardsInDeckCount = self.deck.dynastyDeck.filter({$0.id == card.id}).count
+        let selectedCardsInDeckCount = deck.dynastyDeck.filter({$0.id == card.id}).count
         
         if selectedCardsInDeckCount != numberOfCards {
-            self.deck.dynastyDeck.removeAll(where: {$0.id == card.id})
+            deck.dynastyDeck.removeAll(where: {$0.id == card.id})
             
             if numberOfCards > 0 {
                 var i = 0
                 while i < numberOfCards {
-                    self.deck.dynastyDeck.append(card)
+                    deck.dynastyDeck.append(card)
                     i = i+1
                 }
             }
         }
-        print("There are now \(self.deck.dynastyDeck.filter({$0.id == card.id}).count) copies of \(card.id) in the dynasty deck.")
+        print("There are now \(deck.dynastyDeck.filter({$0.id == card.id}).count) copies of \(card.id) in the dynasty deck.")
         self.tableView.reloadData()
     }
     
     func removeCardTapped(card: Card) {
         print("Remove card button tapped(Tableview)")
-        let cardToRemove: Card = self.deck.dynastyDeck.filter({$0.id == card.id})[0]
-        let firstIndex = self.deck.dynastyDeck.index(of: cardToRemove)
+        let cardToRemove: Card = deck.dynastyDeck.filter({$0.id == card.id})[0]
+        let firstIndex = deck.dynastyDeck.index(of: cardToRemove)
         if let foundCardIndex = firstIndex {
-            self.deck.dynastyDeck.remove(at: foundCardIndex)
-            let cardCount = self.deck.dynastyDeck.filter({$0.id == card.id}).count
+            deck.dynastyDeck.remove(at: foundCardIndex)
+            let cardCount = deck.dynastyDeck.filter({$0.id == card.id}).count
             print("There are now \(cardCount) copies of \(card.id) in the dynasty deck.")
-            self.tableView.reloadData()
+            
+            //Update total card count
+            dynastyDeckCardCount = self.deck.dynastyDeck.count
+            tabBarItem.title = "Dynasty(\(self.dynastyDeckCardCount))"
+            tableView.reloadData()
         }
         
     }
     
     func addCardTapped(card: Card) {
         print("Add card button tapped(Tableview)")
-        self.deck.dynastyDeck.append(card)
-        let cardCount = self.deck.dynastyDeck.filter({$0.id == card.id}).count
+        deck.dynastyDeck.append(card)
+        let cardCount = deck.dynastyDeck.filter({$0.id == card.id}).count
         print("There are now \(cardCount) copies of \(card.id) in the dynasty deck.")
-        self.tableView.reloadData()
-
+        
+        //Update total card count
+        dynastyDeckCardCount = self.deck.dynastyDeck.count
+        tabBarItem.title = "Dynasty(\(self.dynastyDeckCardCount))"
+        tableView.reloadData()
     }
     
 }
